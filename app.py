bf327adbefc48f7f69f23f2b8fa210c99c3f58a9
AJakkam38 <AJakkam38@users.noreply.github.com>
2020-06-30 19:31:53 -0500

    better database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import add_entry, view_entry
+from database import add_entry, get_entries
 
 menu = """Please select one of the folowwing options:
 1) Add new entry for today
@@ -9,12 +9,23 @@ Your selection: """
 
 welcome = "Welcome to the programming jourmal"
 
+def prompt_new_entry():
+    entry_content = input("What have you learned today?")
+    entry_date = input("Enter the date:")
+
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"\n{entry['date']}\n{entry['content']}\n\n")
+
+
 print(welcome)
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        add_entry()
+        prompt_new_entry()
     elif user_input == "2":
-        view_entry()
+        view_entries(get_entries())
     else:
         print("Invalid option, please try again!")
