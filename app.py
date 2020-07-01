dce72300daf3f8c223c06041dd1524a23eab021f
AJakkam38 <AJakkam38@users.noreply.github.com>
2020-07-01 18:38:40 -0500

    working full db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import add_entry, get_entries
+from database import create_table, add_entry, get_entries
 
 menu = """Please select one of the folowwing options:
 1) Add new entry for today
@@ -17,10 +17,11 @@ def prompt_new_entry():
 
 def view_entries(entries):
     for entry in entries:
-        print(f"\n{entry['date']}\n{entry['content']}\n\n")
+        print(f"\n{entry[1]}\n{entry[0]}\n\n")
 
 
 print(welcome)
+create_table()
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
--- database.py
+++ database.py
@@ -3,11 +3,22 @@ import sqlite3
 connection = sqlite3.connect("data.db")
 
 def create_table():
+    # We need context manager when making changes to the DB
     with connection:
-        connection.execute("CREATE TABLE entries(content TEXT, date TEXT);")
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS entries(content TEXT, date TEXT);"
+        )
 
 def add_entry(entry_content, entry_date):
-    entries.append({"content": entry_content, "date": entry_date})
+    with connection:
+        connection.execute(
+            "INSERT INTO entries VALUES(?, ?);", (entry_content, entry_date)
+        )
 
 def get_entries():
-    return entries
+    # We don't need context manager here since we are not making any changes to the DB
+    cursor =  connection.cursor()
+    cursor.execute(
+        "SELECT * FROM entries;"
+    )
+    return cursor
